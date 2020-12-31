5cddb947d0668c2b9b3c0a4e2638c0111f1d1520
wkdcjdfyd <64347947+wkdcjdfyd@users.noreply.github.com>
2020-12-31 22:49:23 +0900

    arrange

---
+++ Class 1/10818.c
@@ -0,0 +1,44 @@
+/*
+	10818번
+
+	문제
+	N개의 정수가 주어진다. 이때, 최솟값과 최댓값을 구하는 프로그램을 작성하시오.
+
+	입력
+	첫째 줄에 정수의 개수 N (1 ≤ N ≤ 1,000,000)이 주어진다. 둘째 줄에는 N개의 정수를 공백으로 구분해서 주어진다. 모든 정수는 -1,000,000보다 크거나 같고, 1,000,000보다 작거나 같은 정수이다.
+
+	출력
+	첫째 줄에 주어진 정수 N개의 최솟값과 최댓값을 공백으로 구분해 출력한다.
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main(){
+
+    int N, a;
+    int i, min, max;
+
+    scanf("%d", &N);
+
+    for(i = 0; i < N; i++){
+
+        scanf("%d",&a);
+
+        if( i == 0 ){
+            min = a;
+            max = a;
+        }
+        else{
+            if(a<min){
+                min = a;
+            }
+            if(a>max){
+                max = a;
+            }
+        } 
+    }
+    printf("%d %d",min,max);
+
+    return 0;
+}
---
+++ Class 1/10951.c
@@ -0,0 +1,27 @@
+/*
+	10951번
+
+	문제
+	두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
+	입력
+	입력은 여러 개의 테스트 케이스로 이루어져 있다.
+
+	각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+	출력
+	각 테스트 케이스마다 A+B를 출력한다.
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+int main(){
+    int a,b;
+    
+    while(scanf("%d %d", &a, &b) != EOF){
+	printf("%d\n", a+b);
+    }
+    
+    return 0;
+}
