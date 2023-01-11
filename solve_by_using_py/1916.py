caa4a3a0cd6685b44b22f3a1e4ca1b754bfe4d3d
Ryong <64347947+wkdcjdfyd@users.noreply.github.com>
2023-01-11 17:29:37 +0900

    1916.py

---
+++ solve_by_using_py/1916.py
@@ -0,0 +1,35 @@
+import sys
+import heapq
+
+input = sys.stdin.readline
+INF = int(1e9)
+
+N = int(input())
+M = int(input())
+
+graph = [[] for i in range(N+1)]
+distance = [INF] * (N+1)
+
+for _ in range(M):
+    x, y, z = map(int, input().split())
+    graph[x].append((y, z))
+
+def dijkstra(start):
+    q = []
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+
+    while q:
+        dist, now = heapq.heappop(q)
+        if distance[now] < dist:
+            continue
+
+        for i in graph[now]:
+            cost = dist + i[1]
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+
+start, end = map(int, input().split())
+dijkstra(start)
+print(distance[end])
