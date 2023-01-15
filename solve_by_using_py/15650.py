4faf2e4d027249451b1da7b6b96a46e9395a8ab4
Ryong <64347947+wkdcjdfyd@users.noreply.github.com>
2023-01-15 23:16:36 +0900

    15650.py

---
+++ solve_by_using_py/15650.py
@@ -0,0 +1,14 @@
+N, M = list(map(int, input().split()))
+arr = []
+
+def dfs(start):
+    if len(arr) == M:
+        print(' '.join(map(str, arr)))
+        return
+
+    for i in range(start, N+1):
+        if i not in arr:
+            arr.append(i)
+            dfs(i + 1)
+            arr.pop()
+dfs(1)
