c0219ffa1d90d46e8eb6170f43136ccfda29868d
Ryong <64347947+wkdcjdfyd@users.noreply.github.com>
2023-02-20 15:19:22 +0900

    3709.py

--- solve_by_using_py/3709.py
+++ solve_by_using_py/3709.py
@@ -7,7 +7,7 @@ for _ in range(T):
     n, r = map(int, input().split())
     loc = [list(map(int, input().split())) for _ in range(r)]
     l_x, l_y = map(int, input().split())
-    
+
     # 레이저 시작점으로부터의 진행방향
     if l_x > n:
         d = 0
@@ -20,23 +20,14 @@ for _ in range(T):
     x = l_x + dx[d]
     y = l_y + dy[d]
 
-    his = []
-    cir_flag = 0
     while 1:
-        if x == 0 or x == n+1 or y == 0 or y == n+1:
+        if x == 0 or x == n + 1 or y == 0 or y == n + 1:
             print(x, y)
             break
         for mir_x, mir_y in loc:
             if x == mir_x and y == mir_y:
-                if (mir_x, mir_y, d) in his:
-                   cir_flag = 1
-                   break
-
-                his.append((mir_x, mir_y, d))
-                d = (d+1) % 4
+                d = (d + 1) % 4
                 break
-        if cir_flag:
-            print(0, 0)
-            break
+
         x = x + dx[d]
         y = y + dy[d]
