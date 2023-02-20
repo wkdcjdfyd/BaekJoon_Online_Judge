a2d02f96b022bb79a62b643c4c9d45e0381124e7
Ryong <64347947+wkdcjdfyd@users.noreply.github.com>
2023-02-20 18:29:11 +0900

    2252.py

---
+++ solve_by_using_py/2252.py
@@ -0,0 +1,34 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+graph = [[] for _ in range(N+1)]
+indegree = [0] * (N+1)
+
+for _ in range(M):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    indegree[b] += 1
+
+def topology_sort():
+    result = []
+    q = deque()
+
+    for i in range(1, N+1):
+        if indegree[i] == 0:
+            q.append(i)
+
+    while q:
+        now = q.popleft()
+        result.append(now)
+
+        for i in graph[now]:
+            indegree[i] -= 1
+            if indegree[i] == 0:
+                q.append(i)
+
+    for i in result:
+        print(i, end=' ')
+
+topology_sort()
