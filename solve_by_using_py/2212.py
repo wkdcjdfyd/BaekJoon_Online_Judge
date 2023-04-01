31beec61c1e85390fe0069a4beb3f0a4760830c2
Ryong <64347947+wkdcjdfyd@users.noreply.github.com>
2023-04-01 17:43:45 +0900

    2212.py

--- solve_by_using_py/2212.py
+++ solve_by_using_py/2212.py
@@ -1,15 +1,11 @@
 N = int(input())
 K = int(input())
-sensor = list(map(int, input().split()))
+sensor = sorted(list(map(int, input().split())))
 
 if K >= N:
     print(0)
 else:
     sensor.sort()
-    dist = [sensor[x+1] - sensor[x] for x in range(N-1)]
-    dist.sort(reverse=True)
+    dist = sorted([sensor[x+1] - sensor[x] for x in range(N-1)], reverse=True)
 
-    for _ in range(K-1):
-        dist.pop(0)
-
-    print(sum(dist))
+    print(sum(dist[K-1:]))
